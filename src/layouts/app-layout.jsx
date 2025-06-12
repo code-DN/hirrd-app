8d371f59cc1d1722262fb85b48c4e24131ed18b9
Devanshu Negi <devanshunegiwork@gmail.com>
2025-06-12 15:51:11 +0530

    Added project files

---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,17 @@
+import Header from "@/components/header";
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <div className="grid-background"></div>
+      <main className="min-h-screen container">
+        <Header />
+        <Outlet />
+      </main>
+
+    </div>
+  );
+};
+
+export default AppLayout;
